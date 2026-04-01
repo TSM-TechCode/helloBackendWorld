d4b55d21dad6ee4a200cc4a4aecd14eed613df6e
TSM-Script <contaquenaoprestaserio@gmail.com>
2026-04-01 18:59:41 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js 
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+api.get('/', (req, res) => {
+  res.send('hello world. Backend rodando 🚀');
+});
+
+api.get('/api', (req, res) => {
+  res.json({ mensagem: 'hello world!' funciona: 'backend rodando...' })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('servidor rodando na porta ' + PORT);
+})
